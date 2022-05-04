37d977fdf1d7e71ce2b3c6cbfacff9a17ad6801f
Icarobernard <icarobernar@hotmail.com>
2022-05-04 20:11:27 -0300

    initial commit

---
+++ pages/api/generate.js
@@ -0,0 +1,24 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const completion = await openai.createCompletion("text-davinci-002", {
+    prompt: generatePrompt(req.body.animal),
+    max_tokens: 180,
+    temperature: 0.7,
+    frequency_penalty: 0.5
+
+  });
+  console.log(completion.data.choices)
+  res.status(200).json({ result: completion.data.choices[0].text });
+}
+
+function generatePrompt(animal) {
+  const capitalizedAnimal =
+    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  return `${capitalizedAnimal}`;
+}
